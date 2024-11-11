b56f8ef96d45d7b3078139790ae5d58d8f78d1c5
Ibrian Danilo Giorgio <ibrian.giorgio@gmail.com>
2024-11-11 00:37:19 -0300

    initial commit

---
+++ src/main/java/com/mercadolibre/mutant_detector/CharBase.java
@@ -0,0 +1,7 @@
+package com.mercadolibre.mutant_detector;
+
+public enum CharBase {
+	
+	A, C, G;
+
+}
---
+++ src/main/java/com/mercadolibre/mutant_detector/MutantAplicattion.java
@@ -0,0 +1,48 @@
+package com.mercadolibre.mutant_detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantAplicattion {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantAplicattion.class, args);
+		
+		String[] dnaMutant = {
+	            "ATGCGA",
+	            "CAGTGC",
+	            "TTATGT",
+	            "AGAAGG",
+	            "CCCCTA",
+	            "TCACTG"
+	        };
+		
+		String[] dnaMutant2 = {
+				  "ATGCGA",
+		          "AYGTGC",
+		          "ATOTGT",
+		          "AGAUZG",
+		          "PCCCTA",
+		          "TCACTG"
+	        };
+		
+
+
+	        String[] dnaNonMutant = {
+	            "ATGCGA",
+	            "CAGTGC",
+	            "TTATTT",
+	            "AGACGG",
+	            "GCGTCA",
+	            "TCACTG"
+	        };
+
+	        System.out.println("Is mutant? " + MutantDetector.isMutant(dnaMutant)); // Debería imprimir: true
+	        System.out.println("Is mutant? " + MutantDetector.isMutant(dnaMutant2)); // Debería imprimir: false
+	        System.out.println("Is mutant? " + MutantDetector.isMutant(dnaNonMutant)); // Debería imprimir: false
+	   
+		
+	}
+
+}
